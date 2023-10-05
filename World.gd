cdca7a99682001728e14f483f08353877811eac8
MalCaor <xav.lemen@orange.fr>
2023-10-05 15:16:51 +0200

    no more while

--- World.gd
+++ World.gd
@@ -25,8 +25,7 @@ func _process(delta):
 func spawn_nour():
 	var new_nour: RigidBody2D = nour.instantiate()
 	var pos: Vector2 = Vector2(0,0)
-	while (pos.x < 5 and pos.x > -5) and (pos.y < 5 and pos.y > -5):
-		pos = Vector2(randf_range(-1,1), randf_range(-1,1)) * 100
+	pos = Vector2(randf_range(-1,1), randf_range(-1,1)) * 100
 	new_nour.transform.origin = pos
 	list_nour.append(new_nour)
 	add_child(new_nour)
